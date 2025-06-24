2028116006e429f026200d08e3f8d821bd8edebc
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-06-24 10:57:19 +0530

    changes in api

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:4000/api/v1";
-const BASE_URL = "https://farmbackend-qwuw.onrender.com";
+const BASE_URL = "https://farmbackend-qwuw.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
