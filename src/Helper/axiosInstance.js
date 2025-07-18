21d244a05dd0f72aa10a6dfaf97d09dfce489bc6
Bhavesh Zalke <zalkebhavesh@gmail.com>
2025-07-18 18:05:19 +0530

    basic changes

--- src/Helper/axiosInstance.js
+++ src/Helper/axiosInstance.js
@@ -6,6 +6,6 @@ const BASE_URL = "https://farmbackend-qwuw.onrender.com/api/v1";
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
-axiosInstance.defaults.withCredentials = true;  
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance; 
